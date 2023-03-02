cbfb9c07de262b33a1db1594fe7faabd2848ea5a
230500226 <125464717+230500226@users.noreply.github.com>
2023-03-02 13:23:56 +0000

    02/mar

--- main.c
+++ main.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+
+int main(void)
+{
+    //Question 1
+    char Name[20] = "Shahied Rustin";
+    char DOB[50] = "August 18, 2003";
+    int Mobile = 630653524;
+
+    printf("Name: %s\n ", Name);
+    printf("DOB: %s\n ", DOB);
+    printf("Mobile: 27-%d\n", Mobile);
+
+    //Question 2
+    int int1,int2;
+    printf("Please input any two numbers: \n");
+    scanf("%d%d", &int1, &int2);
+    int sum = int1 + int2;
+    int diff = int1 - int2;
+    int product = int1 * int2;
+    int quotient = int1 / int2;
+    int modulus = int1 % int2;
+    printf("Sum = %d\n",sum);
+    printf("Diff = %d\n",diff);
+    printf("Product = %d\n",product);
+    printf("Quotient = %d\n",quotient);
+    printf("Modulus = %d\n",modulus);
+
+    //Question 3
+    // 14
+
+    //Question 4
+}
